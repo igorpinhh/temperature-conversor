98ca8efba7b94066107604c451b20a7e495bc54c
Igor Pinheiro <pinheiro-igo21@outlook.com>
2019-10-09 15:08:43 -0300

    now is responsive

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+
+sass.compiler = require("node-sass");
+
+gulp.task('default', watch);
+
+gulp.task("sass", compsass);
+
+function compsass() {
+  return gulp
+    .src("src/scss/styles.scss")
+    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError)) // Converte Sass para CSS mimificado com gulp-sass
+    .pipe(gulp.dest("src/css"));
+}
+
+function watch() {
+  gulp.watch("src/scss/**/*.scss", compsass);
+}
